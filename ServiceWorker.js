168d27c3901cb1c30fa501c8190e7219357e9cd4
mrphilipjoel <39815836+mrphilipjoel@users.noreply.github.com>
2024-01-17 15:31:00 -0500

    build

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "GooglyEyesGames-Boss Battle with Playroom-0.1";
+const contentToCache = [
+    "Build/boss-battle-game.loader.js",
+    "Build/boss-battle-game.framework.js",
+    "Build/boss-battle-game.data",
+    "Build/boss-battle-game.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
